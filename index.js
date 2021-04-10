16a548e16b4dd6abeeff153ec25c7d63cbd25b3b
Karthik Raju <karthikraju391@gmail.com>
2021-04-10 14:32:04 +0000

    design changes

--- index.js
+++ index.js
@@ -3,37 +3,39 @@ const chalk = require('chalk');
 
 var score = 0;
 
-var userName = readlineSync.question(chalk.bgBlue("What is your name? "));
-console.log(chalk.bgBlue("Welcome "+ userName + " to DO YOU KNOW KARTHIK? "));
+var userName = readlineSync.question(chalk.bgCyan("What is your name? "));
+console.log(chalk.bgCyan("Welcome "+ userName + " to DO YOU KNOW KARTHIK? "));
+console.log("--------------");
 
 var levelOne = [
   {
-    question: "Which band do I like? ",
-    answer: "imagine dragons"
+    question: chalk.bgGrey("Which month was I born in? "),
+    answer: "September"
   },
   {
-    question: "What sport do I like? ",
-    answer: "cricket"
+    question: chalk.bgGrey("Do I know to drive a car? "),
+    answer: "yes"
   },
   {
-    question: "Which Crishtopher Nolan movie do I love? ",
-    answer: "interstellar"
+    question: chalk.bgGrey("Do I like sweets? "),
+    answer: "no"
   }
 ]
 
 var levelTwo = [
+
   {
-    question: "Which month was I born in? ",
-    answer: "September"
+    question: chalk.bgGrey("Which band do I like? "),
+    answer: "imagine dragons"
   },
   {
-    question: "Do I like sweets? ",
-    answer: "no"
+    question:chalk.bgGrey("Which Crishtopher Nolan movie do I love? "),
+    answer: "interstellar"
   },
   {
-    question: "Do I know to drive a car? ",
-    answer: "yes"
-  }
+    question:chalk.bgGrey("Which sport do I love to play? "),
+    answer: "cricket"
+  },
 ]
 
 
@@ -51,7 +53,7 @@ function play(question, answer)
     console.log(chalk.red("You are wrong!"));
   }
   console.log("Your score is: ", score);
-  console.log("---------------");
+  console.log("--------------");
 }
 
 
@@ -60,28 +62,29 @@ for(i = 0; i<levelOne.length; i++)
   play(levelOne[i].question, levelOne[i].answer);
 }
 
-console.log("Yay!, you scored: ", score);
+console.log(chalk.bold("Yay!, you scored: ", score));
 
 if(score==3){
   console.log("Wow, you know me well!");
-  console.log("Here is another level for you!");
+  console.log(chalk.yellow("Here is another level for you!"));
+  console.log("--------------");
 
   for(i = 0; i<levelTwo.length; i++){
     play(levelTwo[i].question, levelTwo[i].answer);
   }
   console.log("Your final score is: ", score);
-  console.log("Answers are: ");
+  console.log(chalk.yellow("Answers are: "));
   for(i=0; i<levelOne.length; i++){
     console.log(levelOne[i].answer.toUpperCase());
   }   
   for(i=0; i<levelTwo.length; i++){
     console.log(levelTwo[i].answer.toUpperCase());
   }
-  console.log("I guess you know me a little better now!");
+  console.log(chalk.bgCyan("I guess you know me a little better now!"));
 }
 else{
   console.log(chalk.yellow("Not bad!"));
-  console.log("Answers are: ");
+  console.log(chalk.yellow("Answers are: "));
   for(i=0; i<levelOne.length; i++){
     console.log(levelOne[i].answer.toUpperCase());
   }
